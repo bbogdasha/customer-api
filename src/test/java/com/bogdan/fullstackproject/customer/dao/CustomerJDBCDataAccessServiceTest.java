9cfba85e12b02474632a03b54ef479ae4726b7ef
bogdan <bogdan4888@gmail.com>
2023-10-24 23:59:53 +0300

    tests

---
+++ src/test/java/com/bogdan/fullstackproject/customer/dao/CustomerJDBCDataAccessServiceTest.java
@@ -0,0 +1,316 @@
+package com.bogdan.fullstackproject.customer.dao;
+
+import com.bogdan.fullstackproject.AbstractTestcontainers;
+import com.bogdan.fullstackproject.customer.model.Customer;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CustomerJDBCDataAccessServiceTest extends AbstractTestcontainers {
+
+    private CustomerJDBCDataAccessService underTest;
+
+    private final CustomerRowMapper customerRowMapper = new CustomerRowMapper();
+
+    @BeforeEach
+    void setUp() {
+        underTest = new CustomerJDBCDataAccessService(
+                getJdbcTemplate(),
+                customerRowMapper
+        );
+    }
+
+    @Test
+    void selectAllCustomers() {
+        //Given
+        Customer customer = getCustomer();
+        underTest.insertCustomer(customer);
+
+        //When
+        List<Customer> actual = underTest.selectAllCustomers();
+
+        //Then
+        assertThat(actual).isNotEmpty();
+    }
+
+    @Test
+    void selectCustomerById() {
+        //Given
+        Customer customer = getCustomer();
+        String email = customer.getEmail();
+
+        underTest.insertCustomer(customer);
+
+        int customerId = getCustomerId(email);
+
+        //When
+        Optional<Customer> actual = underTest.selectCustomerById(customerId);
+
+        //Then
+        assertThat(actual).isPresent().hasValueSatisfying(c -> {
+           assertThat(c.getId()).isEqualTo(customerId);
+           assertThat(c.getName()).isEqualTo(customer.getName());
+           assertThat(c.getEmail()).isEqualTo(customer.getEmail());
+           assertThat(c.getAge()).isEqualTo(customer.getAge());
+        });
+    }
+
+    @Test
+    void returnEmptyWhenSelectCustomerById() {
+        //Given
+        int customerId = -1;
+
+        //When
+        Optional<Customer> actual = underTest.selectCustomerById(customerId);
+
+        //Then
+        assertThat(actual).isEmpty();
+    }
+
+    @Test
+    void existsCustomerWithEmail() {
+        //Given
+        Customer customer = getCustomer();
+        String email = customer.getEmail();
+
+        underTest.insertCustomer(customer);
+
+        //When
+        boolean actual = underTest.existsCustomerWithEmail(email);
+
+        //Then
+        assertThat(actual).isTrue();
+    }
+
+    @Test
+    void existsCustomerWithEmailReturnFalseWhenDoesNotExists() {
+        //Given
+        String email = FAKER.internet().safeEmailAddress() + "-" + UUID.randomUUID();
+
+        //When
+        boolean actual = underTest.existsCustomerWithEmail(email);
+
+        //Then
+        assertThat(actual).isFalse();
+    }
+
+    @Test
+    void existsCustomerWithId() {
+        //Given
+        Customer customer = getCustomer();
+        String email = customer.getEmail();
+
+        underTest.insertCustomer(customer);
+
+        int customerId = getCustomerId(email);
+
+        //When
+        boolean actual = underTest.existsCustomerWithId(customerId);
+
+        //Then
+        assertThat(actual).isTrue();
+    }
+
+    @Test
+    void existsCustomerWithIdReturnFalseWhenIdDoesNotExists() {
+        //Given
+        int customerId = -1;
+
+        //When
+        boolean actual = underTest.existsCustomerWithId(customerId);
+
+        //Then
+        assertThat(actual).isFalse();
+    }
+
+    @Test
+    void insertCustomer() {
+    }
+
+    @Test
+    void deleteCustomerById() {
+        //Given
+        Customer customer = getCustomer();
+        String email = customer.getEmail();
+
+        underTest.insertCustomer(customer);
+
+        int customerId = getCustomerId(email);
+
+        //When
+        underTest.deleteCustomerById(customerId);
+
+        //Then
+        Optional<Customer> actual = underTest.selectCustomerById(customerId);
+        assertThat(actual).isNotPresent();
+    }
+
+    @Test
+    void updateCustomerName() {
+        //Given
+        Customer customer = getCustomer();
+        String email = customer.getEmail();
+
+        underTest.insertCustomer(customer);
+
+        int customerId = getCustomerId(email);
+
+        String newName = "Test";
+
+        //When
+        Customer update = new Customer();
+        update.setId(customerId);
+        update.setName(newName);
+
+        underTest.updateCustomer(update);
+
+        //Then
+        Optional<Customer> actual = underTest.selectCustomerById(customerId);
+
+        assertThat(actual).isPresent().hasValueSatisfying(c -> {
+            assertThat(c.getId()).isEqualTo(customerId);
+            assertThat(c.getName()).isEqualTo(newName); //change
+            assertThat(c.getEmail()).isEqualTo(customer.getEmail());
+            assertThat(c.getAge()).isEqualTo(customer.getAge());
+        });
+    }
+
+    @Test
+    void updateCustomerEmail() {
+        //Given
+        Customer customer = getCustomer();
+        String email = customer.getEmail();
+
+        underTest.insertCustomer(customer);
+
+        int customerId = getCustomerId(email);
+
+        String newEmail = FAKER.internet().safeEmailAddress() + "-" + UUID.randomUUID();
+
+        //When
+        Customer update = new Customer();
+        update.setId(customerId);
+        update.setEmail(newEmail);
+
+        underTest.updateCustomer(update);
+
+        //Then
+        Optional<Customer> actual = underTest.selectCustomerById(customerId);
+
+        assertThat(actual).isPresent().hasValueSatisfying(c -> {
+            assertThat(c.getId()).isEqualTo(customerId);
+            assertThat(c.getName()).isEqualTo(customer.getName());
+            assertThat(c.getEmail()).isEqualTo(newEmail); //change
+            assertThat(c.getAge()).isEqualTo(customer.getAge());
+        });
+    }
+
+    @Test
+    void updateCustomerAge() {
+        //Given
+        Customer customer = getCustomer();
+        String email = customer.getEmail();
+
+        underTest.insertCustomer(customer);
+
+        int customerId = getCustomerId(email);
+
+        int newAge = 99;
+
+        //When
+        Customer update = new Customer();
+        update.setId(customerId);
+        update.setAge(newAge);
+
+        underTest.updateCustomer(update);
+
+        //Then
+        Optional<Customer> actual = underTest.selectCustomerById(customerId);
+
+        assertThat(actual).isPresent().hasValueSatisfying(c -> {
+            assertThat(c.getId()).isEqualTo(customerId);
+            assertThat(c.getName()).isEqualTo(customer.getName());
+            assertThat(c.getEmail()).isEqualTo(customer.getEmail());
+            assertThat(c.getAge()).isEqualTo(newAge); //change
+        });
+    }
+
+    @Test
+    void updateAllPropertiesCustomer() {
+        //Given
+        Customer customer = getCustomer();
+        String email = customer.getEmail();
+
+        underTest.insertCustomer(customer);
+
+        int customerId = getCustomerId(email);
+
+        //When
+        Customer update = new Customer();
+        String newEmail = FAKER.internet().safeEmailAddress() + "-" + UUID.randomUUID();
+        update.setId(customerId);
+        update.setName("Test");
+        update.setEmail(newEmail);
+        update.setAge(21);
+
+        underTest.updateCustomer(update);
+
+        //Then
+        Optional<Customer> actual = underTest.selectCustomerById(customerId);
+        assertThat(actual).isPresent().hasValueSatisfying(updated -> {
+            assertThat(updated.getId()).isEqualTo(customerId);
+            assertThat(updated.getName()).isEqualTo("Test");
+            assertThat(updated.getEmail()).isEqualTo(newEmail);
+            assertThat(updated.getAge()).isEqualTo(21);
+        });
+    }
+
+    @Test
+    void willNotUpdateWhenNothingToUpdate() {
+        //Given
+        Customer customer = getCustomer();
+        String email = customer.getEmail();
+
+        underTest.insertCustomer(customer);
+
+        int customerId = getCustomerId(email);
+
+        //When
+        Customer update = new Customer();
+        update.setId(customerId);
+
+        underTest.updateCustomer(update);
+
+        //Then
+        Optional<Customer> actual = underTest.selectCustomerById(customerId);
+
+        assertThat(actual).isPresent().hasValueSatisfying(c -> {
+            assertThat(c.getId()).isEqualTo(customerId);
+            assertThat(c.getName()).isEqualTo(customer.getName());
+            assertThat(c.getEmail()).isEqualTo(customer.getEmail());
+            assertThat(c.getAge()).isEqualTo(customer.getAge());
+        });
+    }
+
+    private Customer getCustomer() {
+        return new Customer(
+                FAKER.name().fullName(),
+                FAKER.internet().safeEmailAddress() + "-" + UUID.randomUUID(),
+                25
+        );
+    }
+
+    private int getCustomerId(String email) {
+        return underTest.selectAllCustomers()
+                .stream()
+                .filter(c -> c.getEmail().equals(email))
+                .map(Customer::getId)
+                .findFirst()
+                .orElseThrow();
+    }
+}
---
+++ src/test/java/com/bogdan/fullstackproject/customer/repository/CustomerRepositoryTest.java
@@ -0,0 +1,119 @@
+package com.bogdan.fullstackproject.customer.repository;
+
+import com.bogdan.fullstackproject.AbstractTestcontainers;
+import com.bogdan.fullstackproject.customer.model.Customer;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * The @DataJpaTest annotation autoconfigures beans for JPA-related components
+ * like our repositories. It also configures beans for things like a DataSource,
+ * JdbcTemplate, or TestEntityManager.
+ * ---
+ * The AutoConfigureTestDatabase.Replace.NONE annotation property tells Spring not
+ * to replace the database with an embedded database. If we didn’t do this,
+ * Spring wouldn’t be using the Postgres data source and would try to autoconfigure
+ * an embedded database instead.
+ * ---
+ * Now our test code will be executed against a PostgreSQL instance running in Docker.
+ * We have also avoided the problems related to using two different database engines.
+ * ---
+ * The @DataJpaTest VS @SpringBootTest
+ * The @SpringBootTest loads full application context, exactly like how you start a
+ * Spring container when you run your Spring Boot application.
+ * The @DataJpaTest loads only configuration for JPA. It uses an embedded in-memory h2
+ * if not specified otherwise.
+ */
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class CustomerRepositoryTest extends AbstractTestcontainers {
+
+    @Autowired
+    private CustomerRepository underTest;
+
+    @BeforeEach
+    void setUp() {
+        underTest.deleteAll();
+    }
+
+    @Test
+    void existsCustomerByEmail() {
+        //Given
+        Customer customer = getCustomer();
+        String email = customer.getEmail();
+
+        underTest.save(customer);
+
+        //When
+        boolean actual = underTest.existsCustomerByEmail(email);
+
+        //Then
+        assertThat(actual).isTrue();
+    }
+
+    @Test
+    void existsCustomerByEmailFailsWhenEmailNotPresent() {
+        //Given
+        String email = FAKER.internet().safeEmailAddress() + "-" + UUID.randomUUID();
+
+        //When
+        boolean actual = underTest.existsCustomerByEmail(email);
+
+        //Then
+        assertThat(actual).isFalse();
+    }
+
+    @Test
+    void existsCustomerById() {
+        //Given
+        Customer customer = getCustomer();
+        String email = customer.getEmail();
+
+        underTest.save(customer);
+
+        int customerId = getCustomerId(email);
+
+        //When
+        boolean actual = underTest.existsCustomerById(customerId);
+
+        //Then
+        assertThat(actual).isTrue();
+    }
+
+    @Test
+    void existsCustomerByIdFailsWhenIdNotPresent() {
+        //Given
+        int customerId = -1;
+
+        //When
+        boolean actual = underTest.existsCustomerById(customerId);
+
+        //Then
+        assertThat(actual).isFalse();
+    }
+
+    private Customer getCustomer() {
+        return new Customer(
+                FAKER.name().fullName(),
+                FAKER.internet().safeEmailAddress() + "-" + UUID.randomUUID(),
+                25
+        );
+    }
+
+    private int getCustomerId(String email) {
+        return underTest.findAll()
+                .stream()
+                .filter(c -> c.getEmail().equals(email))
+                .map(Customer::getId)
+                .findFirst()
+                .orElseThrow();
+    }
+}
