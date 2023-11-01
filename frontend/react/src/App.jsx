76580c0d72a4880c77b3f460bb859bd864c7357e
bogdan <bogdan4888@gmail.com>
2023-11-02 01:25:05 +0200

    Added deploy to aws, js with start ui

---
+++ frontend/react/src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from '@chakra-ui/react'
+import SidebarWithHeader from './shared/SideBar'
+
+const App = () => {
+    return (
+        <SidebarWithHeader>
+            <Button>Click me!!!</Button>
+        </SidebarWithHeader>
+    )
+}
+
+export default App
